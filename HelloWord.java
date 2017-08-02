f1242b54407de2e16463532c7dcef64d2b7dd308
Natalie2101 <natalie.feng@monash.edu>
2017-08-02 18:41:35 +1000

    final

--- HelloWord.java
+++ HelloWord.java
@@ -2,6 +2,7 @@ public class HelloWord
 {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello, Natalie!")
+		System.out.println("Hello, Natalie!");
+		System.out.println("This is the final version!");
 	}
 }
